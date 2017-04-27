4929553f58f96652ac3b983469b8334a97c2ea20
rjd021 <rjd021@shsu.edu>
2017-04-26 23:46:37 -0500

    Update connSpecials.php

--- connSpecials.php
+++ connSpecials.php
@@ -1,26 +1,31 @@
 <?php
-
+//gets db info
 $dbname = "ryanpazu_specials";
 $username = "ryanpazu_BH";
 $password = "BH12345";
 $servername = "server236";
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-//if ($conn->connect_error) {
- //   die("Connection failed: " . $conn->connect_error);
-//} 
 
+//connects to db
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//selecting columns: Venue, Address, Phone from table 'specials'
 $sql = "SELECT Venue, Address, Phone, Specials FROM specials";
+
+//setting result to a connection of the query
 $result = $conn->query($sql);
 
+//if table is not empty
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo"Venue: ". $row["Venue"]. " Address: ". $row["Address"]. " Phone #: ". $row["Phone"]. " Specials: ".  $row["Specials"];
     }
+ //else, table is empty
 } else {
     echo "0 results";
 }
+
+//close connection
 $conn->close();
 ?>
 
